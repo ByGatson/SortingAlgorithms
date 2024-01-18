64a273bbb654aa204d333258d88ba270e2fd58c5
DESKTOP-4665L4T\berkay <bayjava18@gmail.com>
2024-01-18 12:41:31 +0300

    Step-Two

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        ISort sort = new MergeSort();
+        ISort sort = new SelectionSort();
         sort.sort();
-    }
-
-    public static void addTest(TreeManager manager) {
-        manager.add(8);
-        manager.add(3);
-        manager.add(10);
-        manager.add(1);
-        manager.add(6);
-        manager.add(4);
-        manager.add(7);
-        manager.add(14);
-        manager.add(13);
-
-//        manager.pre_order();
-//        manager.in_order();
-//        manager.post_order();
-        manager.level_order();
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,26 +1,24 @@
 public class SelectionSort extends Sort {
-    private int helperIndex = 0;
-    private boolean canIContinue = true;
 
     public SelectionSort() {
         super();
     }
 
     public void sort() {
+        list = new int[]{64, 25, 12, 22, 11, 100, 200, 50, 1, 3, 4, 2};
+        showList();
+        int helperIndex = 0;
         for (int i = 0; i < list.length; i++) {
             int helper = list[i];
             for (int j = i; j < list.length; j++) {
-                if (helper > list[j]) {
+                if (list[j] <= helper) {
                     helper = list[j];
                     helperIndex = j;
-                    canIContinue = true;
                 }
             }
-            if (canIContinue) {
-                list[helperIndex] = list[i];
-                list[i] = helper;
-                canIContinue = false;
-            }
+            int keeper = list[i];
+            list[i] = helper;
+            list[helperIndex] = keeper;
         }
         showList();
     }
